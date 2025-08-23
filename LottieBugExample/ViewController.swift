eac3879ad4e345a10c1003d4c4495859948ec6ca
Sunny <sunnysideupdowntown@gmail.com>
2025-08-23 18:18:19 +0900

    Add lottie view.

--- LottieBugExample/ViewController.swift
+++ LottieBugExample/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    private let lottieView = {
+        let animationView = LottieAnimationView(name: "loading")
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.loopMode = .loop
+        animationView.backgroundBehavior = .continuePlaying
+        animationView.contentMode = .scaleAspectFit
+        animationView.backgroundColor = .clear
+        return animationView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Add lottie view
+        view.addSubview(lottieView)
+        NSLayoutConstraint.activate([
+            lottieView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            lottieView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        // Play lottie view
+        lottieView.play()
     }
-
-
 }
 
